349acd84c8679691db464c3fad9141a155b554cf
Leonardo291024 <guzmanmarcosguzman@outlook.com>
2024-03-24 13:42:01 -0600

    imagenes en carpetas

--- main/lista-proyectos.js
+++ main/lista-proyectos.js
@@ -15,91 +15,91 @@
 const proyectosHtmlCssList = [];
 proyectosHtmlCssList.push({
     name: "To Do List",
-    image: './assets/todoListjpg.jpg',
+    image: './assets/imgsProyects/todoListjpg.jpg',
     readme: "Agrega tareas automaticamente con la opcion de poder borrar una ves que una de las tareas la hayas completado.",
     web: 'https://leonardo291024.github.io/TodoList/',
     repositorio: 'https://github.com/Leonardo291024/TodoList',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
 proyectosHtmlCssList.push({
     name: "Salario Bruto a Salario Neto",
-    image: './assets/salarioBrutojpg.jpg',
+    image: './assets/imgsProyects/salarioBrutojpg.jpg',
     readme: "Bienvenido a este sitio para obtener el salario neto de un empleado a partir de el salario bruto Este proyecto es realizado con base a los temas vistos en el curso de Platzi de la escuela de Desarrollo web (Taller de matematicas y estadistica basica con javascript) donde se vieron temas como traducir formulas matematicas a codigo JS y tambien la manipulacion de objetos y arrays",
     web: 'https://leonardo291024.github.io/sueldo_bruto_a_neto/',
     repositorio: 'https://github.com/Leonardo291024/sueldo_bruto_a_neto',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
 proyectosHtmlCssList.push({
     name: "Calculadora de propinas",
-    image: './assets/calculadoraDePropinas.jpg',
+    image: './assets/imgsProyects/calculadoraDePropinas.jpg',
     readme: "Este es un desafio creado gracias a los desafios de Frontend Mentor. El cual consiste es probar mis habilidades con CSS, HTML JavaScript. Este desafío se centra principalmente en Calcular la propina correcta y el coste total de la factura por persona.Tambien podras ver el comportamiento del sitio en distintos dispositivos ya que es un diseño responsive",
     web: 'https://leonardo291024.github.io/calculadora_de_propinas/',
     repositorio: 'https://github.com/Leonardo291024/calculadora_de_propinas',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
 proyectosHtmlCssList.push({
     name: "Appeperia",
-    image: './assets/apeperia.png',
+    image: './assets/imgsProyects/apeperia.png',
     readme: "pagina web estatica. Practica del curso de Alura Latam/FrontEnd/Grupo5",
     web: 'https://leonardo291024.github.io/Apeperia/',
     repositorio: 'https://github.com/Leonardo291024/Apeperia',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
     iconoJs: ''
 });
 proyectosHtmlCssList.push({
     name: "BatataBit",
-    image: './assets/batataBit.png',
+    image: './assets/imgsProyects/batataBit.png',
     readme: "Proyecto de la escuela de desarrollo web del curso de responsive designe",
     web: 'https://leonardo291024.github.io/Bata-Bit/',
     repositorio: 'https://github.com/Leonardo291024/Bata-Bit',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
     iconoJs: ''
 });
 proyectosHtmlCssList.push({
     name: "Uber",
-    image: './assets/proyecto-uber.jpg',
+    image: './assets/imgsProyects/proyecto-uber.jpg',
     readme: "En este proyecto elabore la copia de la landing page de uber para fines parcticos con tecnologias como HTML y CSS. Consitio en realizar el diseño casi exacto de la web original",
     web: 'https://leonardo291024.github.io/landing-page-uber/',
     repositorio: 'https://github.com/Leonardo291024/landing-page-uber',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
     iconoJs: ''
 });
 proyectosHtmlCssList.push({
     name: "Encriptador De Texto",
-    image: './assets/encriptador.png',
+    image: './assets/imgsProyects/encriptador.png',
     readme: "Esta es una aplicacion donde podras encriptar mensajes y los podras intercanviar con otras personas que sepan el secreto de la encriptacion utilizada.",
     web: 'https://leonardo291024.github.io/textoEncriptador/',
     repositorio: 'https://github.com/Leonardo291024/textoEncriptador',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
 proyectosHtmlCssList.push({
     name: "Calculadora",
-    image: './assets/calculadora.jpg',
+    image: './assets/imgsProyects/calculadora.jpg',
     readme: "Este proyecto consiste en una calculadora basica programada con JavaScript asi como tambien el uso de HTML y CSS.",
     web: 'https://leonardo291024.github.io/calculadora/',
     repositorio: 'https://github.com/Leonardo291024/calculadora',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
 proyectosHtmlCssList.push({
     name: "E-comerce",
-    image: './assets/tienda-empeno.jpg',
+    image: './assets/imgsProyects/tienda-empeno.jpg',
     readme: "Proyecto personal de un sitio web sobre una casa de empeño",
     web: 'https://leonardo291024.github.io/casa-de-empe-o/',
     repositorio: 'https://github.com/Leonardo291024/casa-de-empe-o',
-    iconoHtml: './assets/icons8-html-48.png',
-    iconoCss: './assets/icons8-css-48.png',
-    iconoJs: './assets/icons8-javascript-48.png'
+    iconoHtml: './assets/icons/icons8-html-48.png',
+    iconoCss: './assets/icons/icons8-css-48.png',
+    iconoJs: './assets/icons/icons8-javascript-48.png'
 });
